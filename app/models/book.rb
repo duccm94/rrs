827a0a39ef81cb00bff21e02b8288ec26d2c56c2
hatd <ennino1234@gmail.com>
2016-10-10 08:59:49 +0700

    10/10

--- app/models/book.rb
+++ app/models/book.rb
@@ -7,8 +7,6 @@ class Book < ActiveRecord::Base
 
   validates :title, presence: true, length: {maximum: 50}
   validates :author, presence: true, length: {maximum: 50}
-  validates :number_of_pages, presence: true
-  validates :publish_date, presence: true
 
   Mark.mark_types.keys.each do |name|
     scope :"#{name}_books",
