e71d3f9f882a1c7edc01c377b2cb6750942b42d3
hatd <ennino1234@gmail.com>
2016-11-13 01:23:00 +0700

    fix view 2

--- app/controllers/admin/reviews_controller.rb
+++ app/controllers/admin/reviews_controller.rb
@@ -1,6 +1,7 @@
 class Admin::ReviewsController < ApplicationController
   before_action :logged_as_admin
   before_action :load_book, :load_review, only: [:destroy]
+  after_action :calculate_score, only: :destroy
   def destroy
     if @review && @review.destroy
       flash[:success] = t "controllers.flash.common.destroy_success",
@@ -19,5 +20,15 @@ class Admin::ReviewsController < ApplicationController
 
   def load_review
     @review = Review.find_by id: params[:id]
+  end
+
+  def calculate_score
+    sum = @book.reviews.reduce(0) {|sum, element| sum + element.rating}
+    if @book.reviews.count == 0
+      average_score = 0
+    else
+      average_score = sum / @book.reviews.count
+    end
+    @book.update_attribute :rate_score, average_score
   end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -1,5 +1,7 @@
 class Request < ActiveRecord::Base
   belongs_to :user
+  validates :book_title, presence: true
+  validates :book_author, presence: true
 
   enum accept: [:waiting, :accept, :reject]
 end
